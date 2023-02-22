8a615927e615d3c0fcc8edca611c3a0fac79dbae
Monika Mevenkamp <monikam@princeton.edu>
2023-02-22 18:28:43 +0100

    / says hello

---
+++ src/main/kotlin/com/example/edip/inventory/EdipInventoryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.edip.inventory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EdipInventoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<EdipInventoryApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/edip/inventory/EdipInventoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.edip.inventory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EdipInventoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
