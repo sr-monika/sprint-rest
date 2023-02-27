30c78669b085921133233148ae2184df8826f7be
Monika Mevenkamp <monikam@princeton.edu>
2023-02-27 17:56:58 +0100

    adding service and post

--- src/main/kotlin/com/fedex/spark/edip/inventory/InventoryController.kt
+++ src/main/kotlin/com/fedex/spark/edip/inventory/InventoryController.kt
@@ -1,33 +1,25 @@
 package com.fedex.spark.edip.inventory
 
-import com.fedex.spark.edip.inventory.model.InventoryItem
-import com.fedex.spark.edip.inventory.model.InventoryItemId
-import com.fedex.spark.edip.inventory.model.InventoryLevel
 import com.fedex.spark.edip.inventory.model.InventoryLevelId
-import com.fedex.spark.edip.inventory.model.Money
-import com.fedex.spark.edip.inventory.model.Quantity
-import com.fedex.spark.edip.inventory.model.Sku
-import com.fedex.spark.edip.inventory.model.UtcTimestamp
+import com.fedex.spark.edip.inventory.model.InventoryLevelPostBody
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 
-val item = InventoryItem(
-    InventoryItemId(10), Money(120), true, Sku("UGS-blk-10")
-)
-
-val itemLevel = InventoryLevel(
-    InventoryLevelId(99), item, Quantity(12), 78, effectiveTs = UtcTimestamp(0))
-
 @RestController
 class InventoryController {
-    @GetMapping("/itemlevel/{levelId}")
+    @GetMapping("/inventoryLevel/{levelId}")
     @Operation(description = "Retrieve item level for given id")
-    @ResponseBody
-    fun getItemLevel(@PathVariable levelId: Int): InventoryLevel =
-        itemLevel.copy(id = InventoryLevelId(levelId),
-            effectiveTs = UtcTimestamp(nowTs()))
+    fun getItemLevel(@PathVariable levelId: Int) = getItemLevel(InventoryLevelId(levelId))
+
+
+    @PostMapping("/inventoryLevel/set")
+    @Operation(description = "Adjust level of given sku by given Quantity")
+    fun postItemLevel(@RequestBody request: InventoryLevelPostBody) =
+        InventoryService.setItemLevel(request)
+
 }
---
+++ src/main/kotlin/com/fedex/spark/edip/inventory/db.kt
@@ -0,0 +1,46 @@
+package com.fedex.spark.edip.inventory
+
+import com.fedex.spark.edip.inventory.model.InventoryItem
+import com.fedex.spark.edip.inventory.model.InventoryLevelId
+import com.fedex.spark.edip.inventory.model.InventoryLevelData
+import com.fedex.spark.edip.inventory.model.Money
+import com.fedex.spark.edip.inventory.model.Quantity
+import com.fedex.spark.edip.inventory.model.Sku
+import com.fedex.spark.edip.inventory.model.UtcTimestamp
+
+val itemLevels = HashMap<InventoryLevelId, InventoryLevelData>()
+var itemLevelsId = 0;
+
+fun seedData() {
+    val itemLevelId = InventoryLevelId(0)
+    itemLevels[itemLevelId] =
+        InventoryLevelData(
+            itemLevelId,
+            InventoryItem(Money(120), true, Sku("UGS-blk-10")),
+            Quantity(12),
+            0,
+            UtcTimestamp(0),
+            UtcTimestamp(10)
+        )
+}
+
+
+fun getItemLevel(id: InventoryLevelId) = itemLevels[id]
+
+fun findLevelBySkuLocation(sku: Sku, locationId: Int): InventoryLevelData? =
+    itemLevels.filter { it.value.location_id == locationId && it.value.inventory_item.sku == sku }
+        .firstNotNullOfOrNull { it.value }
+
+
+fun insertItemLevel(data: InventoryLevelData): InventoryLevelData {
+    itemLevelsId += 1
+    itemLevels[InventoryLevelId(itemLevelsId)] = data
+    return data.copy(id = InventoryLevelId(itemLevelsId))
+}
+
+fun updateItemLevel(id: InventoryLevelId, data: InventoryLevelData): InventoryLevelData {
+    val level = data.copy(updatedTs = UtcTimestamp(nowTs()))
+    itemLevels[id] = level
+    return level
+}
+
