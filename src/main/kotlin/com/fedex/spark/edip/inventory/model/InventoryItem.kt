24960613502d244597e2a36018e9c8fca9417c1c
Monika Mevenkamp <monikam@princeton.edu>
2023-02-27 12:58:25 +0100

    fix typo

--- src/main/kotlin/com/fedex/spark/edip/inventory/model/InventoryItem.kt
+++ src/main/kotlin/com/fedex/spark/edip/inventory/model/InventoryItem.kt
@@ -2,8 +2,7 @@ package com.fedex.spark.edip.inventory.model
 
 import io.swagger.v3.oas.annotations.media.Schema
 
-data class InventoryItemId(val id: Int);
-
+data class InventoryItemId(val id: Int)
 
 @Schema
 data class InventoryItem(
